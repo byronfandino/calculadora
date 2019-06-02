56690d72964658896b6939d5d91bd893c8222afa
Byron Fandiño Morales <31166032+byronfandino@users.noreply.github.com>
2019-06-02 09:35:59 -0500

    Programación de reglas del display
    Solo falta programar la última parte de los operandos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var primerChar;
+var caracter;
 var calculadora = {
     teclaSeccion: document.querySelectorAll(".tecla"),
     display: document.getElementById("display"),
@@ -8,16 +8,29 @@ var calculadora = {
 }
 var resultado = calculadora.display;
 
-function validarTeclaNum(num){
+function asignarTeclaNum(num){
     if (resultado.textContent=="0"){
         resultado.innerHTML= num;
     }else{
-        resultado.innerHTML= resultado.textContent + num;
+        //Verificar si el punto se encuentra en alguna posición
+        var punto = resultado.textContent.indexOf(".");
+        
+        //Si el punto no está entonces regresará un valor negativo
+        if (punto<0){
+            if (resultado.textContent.length < 8){
+                resultado.innerHTML= resultado.textContent + num;
+            }
+        }else{
+            //Pero si el punto está ampliamos en 1 la cantidad de digitos aceptada
+            if (resultado.textContent.length < 9){
+                resultado.innerHTML= resultado.textContent + num;
+            }
+        }
     }
 }
 
 function iniciar(){
-    /*---------------Animación reducir tamaño de los botones-------------------*/
+    /*---------------Animación reducir y reestablecer tamaño de los botones-------------------*/
     for (var i = 0; i<calculadora.teclaSeccion.length; i++){
             calculadora.funcionTecla(calculadora.teclaSeccion[i], "mousedown", function(){
                 this.style="border:4px solid transparent";
@@ -27,20 +40,21 @@ function iniciar(){
             })
         }
 
-    /*---------------Asignar el valor de cada tecla------------------------------*/
+    /*---------------Asignar el valor de cada tecla al evento click-------------------------*/
     //tecla ON
     calculadora.funcionTecla(calculadora.teclaSeccion[0], "click", function(){
         resultado.innerHTML= 0;
+        //OJO: Posiblemente hay que vaciar variables globales para suspender las operaciones 
     });
     
     //tecla Cambio de signo
     calculadora.funcionTecla(calculadora.teclaSeccion[1], "click", function(){
         if (resultado.textContent!="0"){
-            primerChar=resultado.textContent;
+            caracter=resultado.textContent;
             //Se verifica si la primera posición contiene el signo -
-            if (primerChar[0]=="-"){
+            if (caracter[0]=="-"){
                 //Se procede a eliminar la primera posición del string
-                resultado.innerHTML = primerChar.substr(1);
+                resultado.innerHTML = caracter.substr(1);
             }else{
                 resultado.innerHTML = "-" + resultado.textContent
             }
@@ -64,7 +78,17 @@ function iniciar(){
     
     //tecla punto
     calculadora.funcionTecla(calculadora.teclaSeccion[16], "click", function(){
-        // falta por asignar
+        
+        //Verificar si el punto se encuentra en alguna posición
+        var punto = resultado.textContent.indexOf(".");
+        
+        //Si el punto no está entonces regresará un valor negativo
+        if (punto<0){
+            if (resultado.textContent.length <= 7){
+                resultado.innerHTML = resultado.textContent + "."
+            }
+        }
+
     });
 
     //tecla igual
@@ -77,54 +101,55 @@ function iniciar(){
         // falta por asignar
     });
     
+    /***NUMEROS***/
     //tecla 7
     calculadora.funcionTecla(calculadora.teclaSeccion[4], "click", function(){
-       validarTeclaNum(7);
+       asignarTeclaNum(7);
     });
     
     //tecla 8
     calculadora.funcionTecla(calculadora.teclaSeccion[5], "click", function(){
-        validarTeclaNum(8);
+        asignarTeclaNum(8);
     });
 
     //tecla 9
     calculadora.funcionTecla(calculadora.teclaSeccion[6], "click", function(){
-        validarTeclaNum(9);
+        asignarTeclaNum(9);
     });
 
     //tecla 4
     calculadora.funcionTecla(calculadora.teclaSeccion[8], "click", function(){
-        validarTeclaNum(4);
+        asignarTeclaNum(4);
     });
     
     //tecla 5
     calculadora.funcionTecla(calculadora.teclaSeccion[9], "click", function(){
-        validarTeclaNum(5);
+        asignarTeclaNum(5);
     });
     
     //tecla 6
     calculadora.funcionTecla(calculadora.teclaSeccion[10], "click", function(){
-        validarTeclaNum(6);
+        asignarTeclaNum(6);
     });
     
     //tecla 1
     calculadora.funcionTecla(calculadora.teclaSeccion[12], "click", function(){
-        validarTeclaNum(1);
+        asignarTeclaNum(1);
     });
     
     //tecla 2
     calculadora.funcionTecla(calculadora.teclaSeccion[13], "click", function(){
-        validarTeclaNum(2);
+        asignarTeclaNum(2);
     });
     
     //tecla 3
     calculadora.funcionTecla(calculadora.teclaSeccion[14], "click", function(){
-        validarTeclaNum(3);
+        asignarTeclaNum(3);
     });
     
     //tecla 0
     calculadora.funcionTecla(calculadora.teclaSeccion[15], "click", function(){
-        validarTeclaNum(0);
+        asignarTeclaNum(0);
     });
 }
 
